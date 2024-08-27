c10ee105157e17653e30e4af726817bbffdcb423
nandaUnical <rtgmfr98t59z504s@studenti.unical.it>
2024-08-27 14:59:15 +0200

    numeros primos y entorno virtual

---
+++ numeros_primos.py
@@ -0,0 +1,7 @@
+def divisores (a):
+    div = [1,a];
+    div += [i for i in range(2, a//2) if a % i == 0]
+    return sorted(div)
+
+def numeros_primos (a):
+    return len(divisores(a)) == 2;
---
+++ pasword_generator.py
@@ -0,0 +1,19 @@
+'''
+Crear ambiente virtual
+
+Implementar un algoritmo para generar constrasenas seguras
+De modo que se pueda personalizar:
+- La longitud de la contrasena
+- Si incluir mayusculas o minusculas
+- Si incluir numeros o caracteres especiales
+- Cuantas constrasenas generar en la misma ejecucion
+- Posibilidad de generar constrasennas a partir de una cadena o subcadena
+
+Hacerle una interfaz grafica con Tkinter
+
+url videos usados:
+https://www.youtube.com/watch?v=TNtrAvNNxTY (entorno virtual)
+https://www.youtube.com/watch?v=mYe-Ju8-D3o&list=PL_wRgp7nihybbJ2vZaVGI5TDdPaK_dFuC&index=82
+
+'''
+

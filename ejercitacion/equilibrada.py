75d6db7cb32c6798e10551d05639912a9ce4fc78
mfaherold1998 <mfaherold1998@gmail.com>
2024-09-01 23:52:10 +0200

    equilibrada complete

---
+++ ejercitacion/equilibrada.py
@@ -0,0 +1,28 @@
+'''
+* Crea un programa que comprueba si los paréntesis, llaves y corchetes
+* de una expresión están equilibrados.
+* - Equilibrado significa que estos delimitadores se abren y cieran
+*   en orden y de forma correcta.
+* - Paréntesis, llaves y corchetes son igual de prioritarios.
+*   No hay uno más importante que otro.
+* - Expresión balanceada: { [ a * ( c + d ) ] - 5 }
+* - Expresión no balanceada: { a * ( c + d ) ] - 5 }
+'''
+
+def balanceada( texto:str ):
+    pila = []
+    diccionario = {')': '(', '}': '{', ']': '['}
+    for elem in texto:
+        if elem in ['(', '{', '[']:            
+            pila.append(elem)
+
+        elif elem in [']', '}', ')']:
+            if diccionario[elem] == pila[-1]:
+                pila.pop()
+            else: 
+                return False
+        
+    if len(pila) == 0:
+        return True
+    
+print (balanceada('{ a * ( c + d ) ] - 5 }'))
